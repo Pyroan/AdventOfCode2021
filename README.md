6fd58f290aa90ece6fcc63cc225d4734b777823f
Evan Schoenberger <evanjschoenberger@gmail.com>
2021-12-02 08:33:46 -0800

    put wayy too much effort into day2graphical

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # Advent Of Code 2021
+
+Every year, I end up adding some extra challenges/projects for myself, for funsies. To make it easier for my future self I'm listing them here
+## Golfs\*:
+- Day 1 (Combined): [97 bytes](day1/day1golf.py)
+
+\* <super>Under my personal rules for this, input must read from a file called "day\*.txt". Golfs can do one or both parts, which will be specified with the filename. Byte counts com from `wc -c`, an files have LF line endings</super>
+## Graphical Versions:
+Trying something new this year, using Löve.
+previously i'd end up spending forever trying to
+figure out weird things with `tkinter` or other
+python builtins.
+If you have it installed you should be able to run each of the files with just `love day*.lua`
+- [Day 2, Part 1](day2/main.lua)
---
+++ day2/main.lua
@@ -0,0 +1,61 @@
+-- only handles part 1
+function love.load()
+    love.window.setMode(1000, 600)
+    love.window.setTitle("Day 2")
+    love.graphics.setFont(love.graphics.newFont(18))
+    legend = love.graphics.newText(love.graphics.newFont(18), "blugh")
+    points = {0, 0}
+    err = ""
+    f = love.filesystem.read("day2.txt")
+    l = {}
+    for a, b in f:gmatch("(%a+) (%d+)") do
+        l[#l + 1] = {a, b}
+    end
+    i = 1
+end
+
+function love.draw()
+
+    -- depth gauge
+    layers = math.ceil(love.graphics.getHeight() / 50)
+    for i = 0, layers do
+        love.graphics.setColor(0, (1 - i / layers) * 0.2, (1 - i / layers) * 0.2)
+        love.graphics.rectangle("fill", 0, i * 50, love.graphics.getWidth(), (i + 1) * 50)
+        love.graphics.setColor(1, 1, 1, 0.2)
+        love.graphics.print(i * 100, 50, i * 50 - 24)
+    end
+    -- legend
+    love.graphics.setColor(0, 0, 0, 0.2)
+    w, h = legend:getDimensions()
+    love.graphics.rectangle("fill", love.graphics.getWidth() / 2 - w / 2 - 10, 35, w + 20, h + 10)
+    love.graphics.setColor(1, 1, 1)
+    love.graphics.draw(legend, math.floor(love.graphics.getWidth() / 2 - legend:getWidth() / 2), 40)
+    -- line
+    love.graphics.setLineStyle("rough")
+    love.graphics.setLineWidth(2)
+    for j = 3, #points, 2 do
+        love.graphics.setColor(5 / (#points - j), 80 / (#points - j), 1, 1)
+        love.graphics.line(points[j - 2] * 0.5, points[j - 1] * 0.5, points[j] * 0.5, points[j + 1] * 0.5)
+    end
+end
+
+function love.update(dt)
+    if i <= #l then
+        x, y = 0, 0
+        if l[i][1] == "forward" then
+            x = l[i][2]
+        elseif l[i][1] == "down" then
+            y = l[i][2]
+        elseif l[i][1] == "up" then
+            y = -l[i][2]
+        end
+        points[#points + 1] = points[#points - 1] + x
+        points[#points + 1] = points[#points - 1] + y
+        i = i + 1
+        legend:set({{0, 1, 1}, "x = " .. points[#points - 1] .. ", y = " .. points[#points]})
+    else
+        legend:set({{0, 1, 1}, "x = " .. points[#points - 1] .. ", y = " .. points[#points] .. "\n", {1, 1, 1},
+                    "Answer: " .. points[#points - 1] * points[#points]})
+    end
+
+end
