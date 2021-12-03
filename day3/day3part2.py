85f6849e56e49234b470c29882aaa864cfee8e8b
Violet Moore <Pyroan@users.noreply.github.com>
2021-12-02 21:42:37 -0800

    cleanup day 3 part 2

--- day3/day3part2.py
+++ day3/day3part2.py
@@ -1,15 +1,15 @@
 with open('day3.txt') as f:
-    l = list(map(str.strip, f.readlines()))
-oxy = l[:]
-i = 0
-while len(oxy) != 1:
-    oxy = list(filter(lambda x: x[i] == ('1' if (sum([int(b[i])
-                                                      for b in oxy]) >= len(oxy)/2) else '0'), oxy))
-    i += 1
-co2 = l[:]
-i = 0
-while len(co2) != 1:
-    co2 = list(filter(lambda x: x[i] == ('0' if (sum([int(b[i])
-                                                      for b in co2]) >= len(co2)/2) else '1'), co2))
-    i += 1
-print(int(oxy[0], 2)*int(co2[0], 2))
+    l = f.readlines()
+
+
+def f(r, s):
+    q = l[:]
+    i = 0
+    while len(q) != 1:
+        q = list(filter(lambda x: x[i] == (r if sum([int(b[i])
+                                                     for b in q]) >= len(q)/2 else s), q))
+        i += 1
+    return int(q[0], 2)
+
+
+print(f('1', '0')*f('0', '1'))
